8c80fcd3958865ecac78256428c73dbac5c38bf2
KendallRey <107897863+KendallRey@users.noreply.github.com>
2025-03-03 13:06:56 +0800

    added initial list view

--- lib/app/features/playlist/presentation/playlist_screen.dart
+++ lib/app/features/playlist/presentation/playlist_screen.dart
@@ -5,6 +5,30 @@ class PlaylistScreenWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      children: [
+        ListView.builder(
+          itemCount: 20,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+                child: ListTile(
+              leading: ClipRRect(
+                borderRadius:
+                    BorderRadius.circular(8), // Adjust the radius as needed
+                child: Image.network(
+                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                  width: 50, // Set a fixed size for consistency
+                  height: 50,
+                  fit: BoxFit
+                      .cover, // Ensures the image covers the area properly
+                ),
+              ),
+              title: Text("Title"),
+              subtitle: Text("by: Sample ft. Test"),
+            ));
+          },
+        )
+      ],
+    );
   }
 }
